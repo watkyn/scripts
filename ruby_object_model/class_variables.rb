776bc3634fbe30d79ee4774abf95d4d34f89b67e
Tony Eichelberger <tony.eich@gmail.com>
2009-12-02 16:04:32 -0600

    added vimrc for safe keeping

---
+++ ruby_object_model/class_variables.rb
@@ -0,0 +1,66 @@
+class Class
+  
+  def do(*syms)
+    syms.each do |sym|      
+      puts sym
+      
+      class_eval <<-EOS
+        def #{sym}
+          puts '#{sym} evaled'
+        end
+      
+      EOS
+      
+    end
+    
+  end
+end
+
+class Polygon
+  
+  @@sides = 10
+  @stuff = 10
+  
+  def self.helper(*any)
+    puts 'did the helper thing'
+  end
+  
+  def self.sides
+    @@sides
+  end
+  
+  def self.stuff
+    @stuff
+  end
+  
+end
+
+class Triangle < Polygon
+  
+  helper :all
+  
+  @@sides = 20
+  @stuff = 4
+end
+
+class Bill < Triangle
+  @@sides = 30
+  @stuff = 5
+end
+
+
+# puts Polygon.stuff
+# puts Triangle.stuff
+# puts Bill.stuff
+# 
+# puts Bill.class_variables
+# puts Polygon.instance_variables
+
+Triangle.do(:tony, :mindy, { :anth => 3, :joey => 4 })
+
+t = Triangle.new
+
+t.tony
+
+
+
