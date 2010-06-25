d55df2ffea9b623dbb52e7f8e6d1c6268035552b
Tony Eichelberger <tony.eich@gmail.com>
2010-06-25 12:47:02 -0500

    empty

--- clojure/fun.clj
+++ clojure/fun.clj
@@ -1,2 +1,12 @@
 (println (.toUpperCase "hello"))
 (println (+ 1 2 3))
+
+
+(defn square [x] (println(* x x)))
+
+(defn sum-of-squares [x, y] 
+  (println(+ (square x) (square y) )))
+
+(square 21)
+(sum-of-squares 2 3)
+
