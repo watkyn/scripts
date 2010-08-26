e1d2e9e683fffb96426daabe7e71aa7415ebb8de
Tony <watkyn@gmail.com>
2010-08-26 17:07:49 -0500

    .

---
+++ ruby/episode_7/method_added.rb
@@ -0,0 +1,12 @@
+
+class Tony
+
+  def self.method_added(name)
+    puts name
+  end
+
+  def tony
+  end
+end
+
+
---
+++ ruby/episode_7/trace.rb
@@ -0,0 +1,36 @@
+module Trace
+  
+  def self.included(klass)
+    def klass.method_added(name)
+      return if @_adding_method_right_now
+      @_adding_method_right_now = true
+      old_method = self.instance_method(name)
+      define_method name do |*args, &block|
+        puts "==> calling #{name} with #{args.inspect}"
+        result = old_method.bind(self).call(*args, &block)
+        puts "<== result is #{result}"
+        result
+      end
+      @_adding_method_right_now = false
+    end
+  end
+
+end
+
+class TraceExample
+  include Trace
+
+  def some_method(arg1, arg2)
+    arg1 + arg2
+  end
+  
+  def with_block(arg, &block)
+    block.call * arg
+  end
+
+end
+
+ex = TraceExample.new
+ex.some_method(2, 3)
+ex.with_block(4) { 3 }
+
